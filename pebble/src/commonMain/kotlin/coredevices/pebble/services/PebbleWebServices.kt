eb273f95c163ee9858a676bd135683a2b7349325
Steve Penna <steve@penna.org.uk>
2026-08-06 17:59:36 -0400

    Fix previous commit

--- pebble/src/commonMain/kotlin/coredevices/pebble/services/PebbleWebServices.kt
+++ pebble/src/commonMain/kotlin/coredevices/pebble/services/PebbleWebServices.kt
@@ -982,7 +982,6 @@ fun StoreApplication.toLockerEntry(sourceUrl: String, timelineToken: String?): L
         companions = app.companions,
         category = app.category,
         userToken = timelineToken,
-        source = app.source,
         hardwarePlatforms = buildList {
             var fallbackFlags = 0
             if (app.type == AppType.Watchface.code) {
